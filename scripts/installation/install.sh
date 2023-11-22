de19b236cfa66a388819eba386610a972d2be4c3
Anton Bushuiev <anton.bushuiev@gmail.com>
2023-11-22 09:03:33 +0100

    Minor fix

--- scripts/installation/install.sh
+++ scripts/installation/install.sh
@@ -10,7 +10,7 @@ pip install cmake
 pip install git+https://github.com/pyg-team/pyg-lib.git
 pip install torch_geometric==2.3.1
 pip install torch-scatter torch-sparse
-pip install pytorch_lightning==1.9.0
+pip install pytorch_lightning==2.0.8
 
 # Install dependencies
 pip install git+https://github.com/a-r-j/graphein.git@master
@@ -18,8 +18,8 @@ pip install equiformer-pytorch==0.3.9
 pip install wandb
 pip install hydra-core
 pip install -U hydra-submitit-launcher
-pip install -e ../PPIRef-ICLR2024-rebuttal.git
-pip install -e ../mutils-ICLR2024-rebuttal-.git
+pip install -e ../PPIRef-ICLR2024-rebuttal
+pip install -e ../mutils-ICLR2024-rebuttal-
 
 # Install current project
 pip install -e .
